b66ad250e1eeaa362b3c429b0a041e52cd579037
DonitaEnojada13 <donitaprogramadora@gmail.com>
2026-04-23 20:45:42 -0600

    Metodo agrega en Rojinegros

--- ArbolBinarioRojinegro.java
+++ ArbolBinarioRojinegro.java
@@ -12,7 +12,7 @@ public class ArbolBinarioRojinegro<T extends Comparable<T>> {
 
 	public VerticeRn(T elemento) {
 	    this.elemento = elemento;
-	    this.color = Color.ROJO;
+	    this.color = Color.NINGUNO;
 	}
 
 	/**
@@ -139,10 +139,23 @@ public class ArbolBinarioRojinegro<T extends Comparable<T>> {
      * Metodo privado que emula la introduccion de ArbolOrdenado, donde los elementos 
      * menores o iguales van a a la izquierda y los mayores a la derecha 
      */
-
+    
     public void agregaRn(T elemento) {
-
+	meteAB(elemento);
+	ultimoAgregado.color = Color.ROJO;
+	rebalanceoMete(ultimoAgregado);
     }
+
+    private void meteAB(T elemento) {
+        if (elemento == null)
+            throw new IllegalArgumentException();
+        if (raiz == null)
+            raiz = ultimoAgregado = new VerticeRn(elemento);
+        else
+            meteAB(raiz, elemento);
+        elementos++;
+    }
+    
     private void meteAB(VerticeRn dentro, T  mete) {
 	
 		if(mete.compareTo(dentro.get()) <= 0) {
@@ -164,10 +177,71 @@ public class ArbolBinarioRojinegro<T extends Comparable<T>> {
 			}
 		}
     }
+<<<<<<< HEAD
     
     private void rebalanceo(){
 
 	}
+=======
+>>>>>>> bfa1738 (Metodo agrega Rojinegro)
 
+    private void rebalanceoMete(VerticeRn a) {
+
+	// Caso 1 el padre es null, se pinta de negro y terminamos 
+	if (!a.hayPadre()) {
+	    raiz = a;
+	    a.color = Color.NEGRO;
+	    return;
+	}
+
+	// Caso 2, el padre era negro, termina el algoritmo
+	if (a.padre.getColor() == Color.NEGRO) {
+	    return;
+	}
+
+	//Caso 3 tio rojo y padre rojo, se pintan de negro, abuelo de rojo y recursion sobre el abuelo
+	// Tremenda mierda de metodo, quedaria mejor de otra manera
+
+	// Si el tio != null y el tio es rojo, se pinta al tio de y al padre de rojo y al abuelo de negro
+	// despues se hace recursion sobre el abuelo
+
+	
+	VerticeRn tio = esHijoIzquierdo(a.padre())
+	    ? a.padre().padre().derecho()
+	    : a.padre().padre().izquierdo();
+	
+	if (tio != null && tio.getColor() == Color.ROJO) {
+	    tio.color = Color.NEGRO;
+	    a.padre().color = Color.NEGRO;
+	    a.padre().padre().color = Color.ROJO;
+	    rebalanceoMete(a.padre().padre());
+	    return;
+	}
+
+	// Caso 4 estan cruzados, aqui entra en accion el XOR
+
+	// Tengo entendido que si los dos cumplen en ser izq, se va al caso 5
+	// Si los dos son falsos, se va al caso 5
+	// Si uno es falso, caso 4
+	// El XOR lo que hace es separar lo anterior 
+	// A ver si soy capaza de describir como funciona esto
+	VerticeRn padre = a.padre();
+	if (esHijoIzquierdo(a) ^ esHijoIzquierdo(padre)) {
+	    if (esHijoIzquierdo(padre))
+		giroI(padre);
+	    else
+		giroD(padre);
+	    VerticeRn aux = a;
+	    a = padre;
+	    padre = aux;
+	}
+	// Caso 5 ya estan descruzados, se pintan y se gira el abuelo
+	padre.color = Color.NEGRO;
+	a.padre().padre().color = Color.ROJO;
+	if (esHijoIzquierdo(a))
+	    giroD(a.padre().padre());
+	else
+	    giroI(a.padre().padre());
+    }
     
 }
